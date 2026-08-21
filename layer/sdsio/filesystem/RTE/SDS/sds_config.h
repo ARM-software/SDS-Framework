315900faf19ceb35c75fc19c17a6c8024f900e18
MiloradCvjetkovic <milorad.cvjetkovic@arm.com>
2026-08-21 11:14:13 +0200

    Layers: Update sds_config.h file

--- layer/sdsio/filesystem/RTE/SDS/sds_config.h
+++ layer/sdsio/filesystem/RTE/SDS/sds_config.h
@@ -40,7 +40,7 @@
 #define SDS_THREAD_STACK_SIZE           1024
 
 // SDS system thread priority
-#define SDS_THREAD_PRIORITY             osPriorityNormal
+#define SDS_THREAD_PRIORITY             osPriorityNormal1
 
 // SDS stream open timeout in kernel ticks
 #define SDS_OPEN_TIMEOUT                3000U
